9c0f6897fb27ab564e150ad3d804dbdbdec091d9
kongr45gpen <electrovesta@gmail.com>
2017-09-08 22:13:29 +0300

    Add minor improvements and more controls

--- ir_stepper_smallsteps/ir_stepper_smallsteps.ino
+++ ir_stepper_smallsteps/ir_stepper_smallsteps.ino
@@ -11,11 +11,14 @@
 int  Steps2Take;  // 2048 = 1 Revolution
 int receiver = 6; // Signal Pin of IR receiver to Arduino Digital Pin 6
 const int STEPPER_PINS[] = { 8, 10, 9, 11};
+int stepsToTake = 256;
 
 /*-----( Declare objects )-----*/
 // Setup of proper sequencing for Motor Driver Pins
 // In1, In2, In3, In4 in the sequence 1-3-2-4
 // 5.62 degrees / 64 steps
+
+const float degsPerStep = 5.625/64.0;
 
 Stepper small_stepper(STEPS, 8, 10, 9, 11);
 #if !DEBUG
@@ -24,6 +27,7 @@ decode_results results;     // create instance of 'decode_results'
 #else
 struct { int value; } results;
 #endif
+long currentPosition = 0;
 
 void setup()
 {
@@ -32,10 +36,17 @@ void setup()
   #endif
   Serial.begin(9600);
   pinMode(13, OUTPUT);
+  small_stepper.setSpeed(700);
 }
 
 void loop()
 {
+  while(digitalRead(5) == HIGH) {
+    small_stepper.step(2);
+  }
+  while(digitalRead(6) == HIGH) {
+    small_stepper.step(-2);
+  }
   #if DEBUG
   if (digitalRead(2) == HIGH || digitalRead(3) == HIGH || digitalRead(4) == HIGH) { // have we received an IR signal?
     if (digitalRead(2) == HIGH) results.value = 16;
@@ -46,32 +57,36 @@ void loop()
     while(digitalRead(4) == HIGH) {}
   #else
   if (irrecv.decode(&results)) { // have we received an IR signal?
+  // add infrared signal to override
   #endif
     switch (results.value) {
       case 16: // UP button pressed
-        small_stepper.setSpeed(700); //Max seems to be 700
-        Steps2Take  =  256;  // Rotate CW
-        small_stepper.step(Steps2Take);
+        small_stepper.step(stepsToTake);
+        currentPosition += stepsToTake;
         break;
 
       case 2065: // DOWN button pressed
-        small_stepper.setSpeed(700);
-        Steps2Take  =  -256;  // Rotate CCW
-        small_stepper.step(Steps2Take);
+        small_stepper.step(-stepsToTake);
+        currentPosition -= stepsToTake;
         break;
 
       case 107: // RESET button pressed
         digitalWrite(13, HIGH);
-        small_stepper.setSpeed(160);
-        small_stepper.step(30);delay(100);small_stepper.step(-30);
+        while(currentPosition != 0) {
+          if (currentPosition > 0) {
+            small_stepper.step(-1);
+            currentPosition -= 1;
+          } else {
+            small_stepper.step(1);
+            currentPosition += 1;
+          }
+        }
         digitalWrite(13, LOW);
         break;
 
     }
 
-    // Disable stepper motor
-    digitalWrite(9, LOW);
-    digitalWrite(8, LOW);
+   
     Serial.println(results.value);
 
     #if !DEBUG
@@ -79,4 +94,10 @@ void loop()
     #endif
   }
 
+  // Disable stepper motor
+    digitalWrite(11,LOW);
+    digitalWrite(10,LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
 }
+
