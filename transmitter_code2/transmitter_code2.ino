bc523716af83f3af4af302f103ad02cd3577570b
kongr45gpen <electrovesta@gmail.com>
2017-09-09 01:04:47 +0300

    BROKEN CODE

--- transmitter_code2/transmitter_code2.ino
+++ transmitter_code2/transmitter_code2.ino
@@ -1,6 +1,27 @@
 #include <DHT.h>
 #include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
+#include <Stepper.h>
+#include <IRremote.h>
+// TODO: fix breaking DHT
+
+/*----- Variables, Pins -----*/
+#define STEPS  32   // Number of steps per revolution of Internal shaft
+int receiver = 7; // Signal Pin of IR receiver to Arduino Digital Pin
+int stepsToTake = 256; // 2048 = 1 Revolution -- Number of steps to take every message
+
+/*-----( Declare objects )-----*/
+// Setup of proper sequencing for Motor Driver Pins
+// In1, In2, In3, In4 in the sequence 1-3-2-4
+// 5.62 degrees / 64 steps
+
+const float degsPerStep = 5.625 / 64.0;
+
+Stepper small_stepper(STEPS, 8, 10, 9, 11);
+IRrecv irrecv(receiver);    // create instance of 'irrecv'
+decode_results results;     // create instance of 'decode_results'
+
+long currentPosition = 0;
 
 RH_ASK driver;
 
@@ -24,43 +45,88 @@ float lvl; // Stores level value
 
 void setup()
 {
-    Serial.begin(9600);   // Debugging only
-    dht.begin();
-    if (!driver.init())
-         Serial.println("init failed");
+  Serial.begin(9600);   // Debugging only
+  dht.begin();
+  if (!driver.init())
+    Serial.println("init failed");
+
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(13, OUTPUT);
+  small_stepper.setSpeed(700);
 }
-  
+
 void loop()
 {
-    //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp= dht.readTemperature();
+  // Adjust the stepper if requested
+  while (digitalRead(5) == HIGH) small_stepper.step(2);
+  while (digitalRead(6) == HIGH) small_stepper.step(-2);
 
-    // Convert to integer and fractional parts
-    double tempInt;
-    float tempFrac = modf(temp, &tempInt);
-    double batInt;
-    float batFrac = modf(temp, &batInt);
-    double lvlInt;
-    float lvlFrac = modf(temp,&lvlInt);
+  digitalWrite(13, HIGH);
+  //Read data and store it to variables hum and temp
+  hum = dht.readHumidity();
+  temp = dht.readTemperature();
+  digitalWrite(13, LOW);
 
-    Serial.print(tempInt); Serial.print("."); Serial.println(tempFrac*256);
-    
-    const int8_t buffer[8] = {
-        -127, // an identifying value that will not show up in the data
-              // so that we know when the transmission starts
-        (int8_t) tempInt, // this assumes temperature is between -126 and 127
-        (int8_t) (tempFrac*127), // convert the fractional part to an integer -
-                                 // the receiver will have to decode this
-        (int8_t) hum, // humidity is always between 0 and 100
-        (int8_t) batInt, // battery is between 0 and 127
-        (int8_t) (batFrac*127),
-        (int8_t) lvlInt, // level is between 0 and 90
-        (int8_t) (lvlFrac*127),
-    };
-     
-    driver.send((uint8_t *)buffer, 4);
-    driver.waitPacketSent();
+  // Convert to integer and fractional parts
+  double tempInt;
+  float tempFrac = modf(temp, &tempInt);
+  double batInt;
+  float batFrac = modf(temp, &batInt);
+  double lvlInt;
+  float lvlFrac = modf(temp, &lvlInt);
+
+  Serial.print(hum);
+  Serial.print(" ");
+  Serial.println(temp);
+
+  const int8_t buffer[8] = {
+    -127, // an identifying value that will not show up in the data
+    // so that we know when the transmission starts
+    (int8_t) tempInt, // this assumes temperature is between -126 and 127
+    (int8_t) (tempFrac * 127), // convert the fractional part to an integer -
+    // the receiver will have to decode this
+    (int8_t) hum, // humidity is always between 0 and 100
+    (int8_t) batInt, // battery is between 0 and 127
+    (int8_t) (batFrac * 127),
+    (int8_t) lvlInt, // level is between 0 and 90
+    (int8_t) (lvlFrac * 127),
+  };
+
+  if (irrecv.decode(&results) && 0) { // have we received an IR signal?
+    switch (results.value) {
+      case 16: // UP button pressed
+      case 1153:
+        small_stepper.step(stepsToTake);
+        currentPosition += stepsToTake;
+        break;
+
+      case 2065: // DOWN button pressed
+      case 3201:
+        small_stepper.step(-stepsToTake);
+        currentPosition -= stepsToTake;
+        break;
+
+      case 97: // RESET button pressed
+        digitalWrite(13, HIGH);
+        while (currentPosition != 0) {
+          if (currentPosition > 0) {
+            small_stepper.step(-1);
+            currentPosition -= 1;
+          } else {
+            small_stepper.step(1);
+            currentPosition += 1;
+          }
+        }
+        digitalWrite(13, LOW);
+        break;
+    }
+    irrecv.resume(); // receive the next value
+  }
+
+  driver.send((uint8_t *)buffer, 4);
+    digitalWrite(13, HIGH);
+    //driver.waitPacketSent();
+    digitalWrite(13, LOW);
     delay(500);
 }
 
