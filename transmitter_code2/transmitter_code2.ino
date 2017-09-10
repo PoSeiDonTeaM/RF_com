ccaf1c1d1e699a3deb6d1b62a10c739119e3d80c
kongr45gpen <electrovesta@gmail.com>
2017-09-10 06:39:44 +0300

    Minimal version

--- transmitter_code2/transmitter_code2.ino
+++ transmitter_code2/transmitter_code2.ino
@@ -1,14 +1,12 @@
-#include <DHT.h>
-#include <RH_ASK.h>
 #include <SPI.h> // Not actually used but needed to compile
-#include <Stepper.h>
 #include <IRremote.h>
+#include <Stepper.h>
 // TODO: fix breaking DHT
 
 /*----- Variables, Pins -----*/
 #define STEPS  32   // Number of steps per revolution of Internal shaft
 int receiver = 7; // Signal Pin of IR receiver to Arduino Digital Pin
-int stepsToTake = 64; // 2048 = 1 Revolution -- Number of steps to take every message
+int stepsToTake = 16; // 2048 = 1 Revolution -- Number of steps to take every message
 const float maxvolts = 12;
 const float minvolts = 4;
 
@@ -23,15 +21,9 @@ const float beta   = - minvolts * lambda;
 
 Stepper small_stepper(STEPS, 8, 10, 9, 11);
 IRrecv irrecv(receiver);    // create instance of 'irrecv'
-decode_results results;     // create instance of 'decode_results'
+decode_results results;
 
 long currentPosition = 0;
-
-RH_ASK driver;
-
-#define DHTPIN 2     // what pin we're connected to
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 // DHT sensor has a 0.5 Hz sampling rate,
 // but we are transmitting data faster to
@@ -50,11 +42,12 @@ float lvl; // Stores level value
 void setup()
 {
   Serial.begin(9600);   // Debugging only
-  dht.begin();
-  if (!driver.init())
-    Serial.println("init failed");
 
   irrecv.enableIRIn(); // Start the receiver
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
   pinMode(13, OUTPUT);
   small_stepper.setSpeed(700);
 }
@@ -76,33 +69,30 @@ void loop()
   */
 
   if (irrecv.decode(&results)) { // have we received an IR signal?
+    digitalWrite(13, HIGH);
     switch (results.value) {
       case 16: // UP button pressed
       case 1153:
-        small_stepper.step(-stepsToTake);
-        currentPosition -= stepsToTake;
+        small_stepper.step(stepsToTake);
+        currentPosition += stepsToTake;
         break;
 
       case 2065: // DOWN button pressed
       case 3201:
-        small_stepper.step(+stepsToTake);
-        currentPosition += stepsToTake;
+        small_stepper.step(-stepsToTake/4);
+        currentPosition -= stepsToTake/4;
         break;
 
       case 97: // RESET button pressed
-        digitalWrite(13, HIGH);
-        while (currentPosition != 0) {
-          if (currentPosition > 0) {
-            small_stepper.step(-1);
-            currentPosition -= 1;
-          } else {
-            small_stepper.step(1);
-            currentPosition += 1;
-          }
-        }
-        digitalWrite(13, LOW);
+        small_stepper.step(512);
         break;
+      case 2689: // POWER button pressed
+        small_stepper.step(-256);
+        break;
+
     }
+    digitalWrite(13, LOW);
+    Serial.println(results.value);
     irrecv.resume(); // receive the next value
   }
 
@@ -117,39 +107,7 @@ void loop()
     lastTransmission = millis();
 
     digitalWrite(13, HIGH);
-    //Read data and store it to variables hum and temp
-    hum = dht.readHumidity();
-    temp = dht.readTemperature();
-    digitalWrite(13, LOW);
-
-    // Convert to integer and fractional parts
-    double tempInt;
-    float tempFrac = modf(temp, &tempInt);
-    double batInt;
-    float batFrac = modf(temp, &batInt);
-    double lvlInt;
-    float lvlFrac = modf(currentPosition * degsPerStep, &lvlInt);
-
-    Serial.print(hum);
-    Serial.print(" ");
-    Serial.println(temp);
-
-    const int8_t buffer[8] = {
-      -127, // an identifying value that will not show up in the data
-      // so that we know when the transmission starts
-      (int8_t) tempInt, // this assumes temperature is between -126 and 127
-      (int8_t) (tempFrac * 127), // convert the fractional part to an integer -
-      // the receiver will have to decode this
-      (int8_t) hum, // humidity is always between 0 and 100
-      (int8_t) lvlInt, // level is between 0 and 90
-      (int8_t) (lvlFrac * 127),
-      (int8_t) batInt, // battery is between 0 and 127
-      (int8_t) (batFrac * 127)
-    };
-
-    driver.send((uint8_t *)buffer, 8);
-    digitalWrite(13, HIGH);
-    driver.waitPacketSent();
+    //delay(1);
     digitalWrite(13, LOW);
   }
 }
