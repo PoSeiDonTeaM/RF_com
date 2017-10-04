4917ea6692bfba6ea153ae676b42ef1e33f4211d
kongr45gpen <electrovesta@gmail.com>
2017-10-04 21:28:56 +0300

    Add a GUI to the linux receiver

--- linux_receiver/main.cpp
+++ linux_receiver/main.cpp
@@ -12,7 +12,18 @@
 #include <boost/asio.hpp>
 #include <fstream>
 
-const int update_ms = 300;
+#include "lib/imgui/imgui.h"
+#include "imgui_impl_glfw.h"
+#include <stdio.h>
+#include <GLFW/glfw3.h>
+
+const int GRAPH_SIZE = 100;
+
+const int update_ms = 100;
+
+static void error_callback(int error, const char* description) {
+    fprintf(stderr, "Error %d: %s\n", error, description);
+}
 
 int main() {
     std::cout << "Hello, Worlds" << std::endl;
@@ -40,6 +51,33 @@ int main() {
     // Prepare insertion statements
     stMagnetic = con->prepareStatement("INSERT INTO mag(value,time) VALUES(?,NOW(2))     ON DUPLICATE KEY UPDATE value = ?");
     stSignal   = con->prepareStatement("INSERT INTO `signal`(value,time) VALUES(?,NOW(2))  ON DUPLICATE KEY UPDATE value = ?");
+
+
+    // Setup window
+    glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        return 1;
+    GLFWwindow* window = glfwCreateWindow(1280, 720, "ImGui OpenGL2 example", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup ImGui binding
+    ImGui_ImplGlfw_Init(window, true);
+
+    // Load Fonts
+    // (there is a default font, this is only if you want to change it. see extra_fonts/README.txt for more details)
+    ImGuiIO& io = ImGui::GetIO();
+    //io.Fonts->AddFontDefault();
+//    io.Fonts->AddFontFromFileTTF("../lib/imgui/extra_fonts/Cousine-Regular.ttf", 15.0f);
+    io.Fonts->AddFontFromFileTTF("../lib/imgui/extra_fonts/DroidSans.ttf", 16.0f);
+//    io.Fonts->AddFontFromFileTTF("../lib/imgui/extra_fonts/ProggyClean.ttf", 13.0f);
+//    io.Fonts->AddFontFromFileTTF("../lib/imgui/extra_fonts/ProggyTiny.ttf", 10.0f);
+    //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, NULL, io.Fonts->GetGlyphRangesJapanese());
+
+    bool show_test_window = true;
+    ImVec4 clear_color = ImColor(35, 44, 59);
+
+    float magneticData[GRAPH_SIZE] = { 0 };
 
     try {
         // Serial interface initialisation
@@ -91,6 +129,12 @@ int main() {
                         stSignal->execute();
 
                         last_update = std::chrono::steady_clock::now();
+
+                        // Move data back
+                        for (int i = 1; i < GRAPH_SIZE; i++) {
+                            magneticData[i-1] = magneticData[i];
+                        }
+                        magneticData[GRAPH_SIZE - 1] = norm;
                     }
 
                     std::this_thread::sleep_for(std::chrono::milliseconds(5));
@@ -98,6 +142,32 @@ int main() {
                     if (line == "stop") {
                         break;
                     }
+
+                    glfwPollEvents();
+                    ImGui_ImplGlfw_NewFrame();
+                    if (show_test_window) {
+                        ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiSetCond_FirstUseEver);
+                        ImGui::ShowTestWindow(&show_test_window);
+                    }
+
+                    ImGui::Begin("ASAT CubeSAT");
+
+                    ImGui::Checkbox("Test", &show_test_window);
+
+                    ImGui::Text("Magnetic Field Strength (uT)");
+                    ImGui::PlotLines("Curve", magneticData, GRAPH_SIZE, 0, nullptr, FLT_MAX, FLT_MAX, ImVec2(0,80));
+
+                    ImGui::End();
+
+                    // Rendering
+                    int display_w, display_h;
+                    glfwGetFramebufferSize(window, &display_w, &display_h);
+                    glViewport(0, 0, display_w, display_h);
+                    glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+                    glClear(GL_COLOR_BUFFER_BIT);
+                    //glUseProgram(0); // You may want this if using this code in an OpenGL 3+ context where shaders may be bound
+                    ImGui::Render();
+                    glfwSwapBuffers(window);
                 } else {
                     std::this_thread::sleep_for(std::chrono::milliseconds(5));
                 }
@@ -114,6 +184,10 @@ int main() {
     delete stMagnetic;
     delete stSignal;
     delete con;
+
+    // Cleanup
+    ImGui_ImplGlfw_Shutdown();
+    glfwTerminate();
 
     return 0;
 }
