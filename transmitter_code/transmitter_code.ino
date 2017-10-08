fdaa568d4ad4facaff6689c36b3631b47f50462b
kongr45gpen <electrovesta@gmail.com>
2017-10-08 04:16:48 +0300

    Add voltage sensing code to the transmitter

--- transmitter_code/transmitter_code.ino
+++ transmitter_code/transmitter_code.ino
@@ -31,13 +31,13 @@ int chk;
 // Magnetic field: uT (microTesla)
 // Temperature: Celsius
 // Pressure: ???
-// Battery: ???
+// Battery: Volts
 float mag_x; // Magnetic field X value
 float mag_y; // Magnetic field Y value
 float mag_z; // Magnetic field Z value
 float temp = 25.2; // Temperature
 float pres = 25.1; // Pressure
-float bat = 77.8; // Battery charge level
+float bat = 5; // Battery charge level
 
 void setup()
 {
@@ -77,6 +77,35 @@ void magneticLoop()
   Serial.print(" ");
   Serial.print(mag_z);
   Serial.println();*/
+}
+
+/*
+ * Battery voltage acquisition
+ */
+void batteryLoop() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+
+  bat = ((float) result) / 1000.0;
 }
 
 /**
@@ -129,6 +158,7 @@ void loop()
   if (millis() % 65535 - lastTransmission > 1000.0/transmissions_per_second) {
     lastTransmission = millis();
     magneticLoop();
+    batteryLoop();
 
     rfLoop();
     digitalWrite(13, HIGH);
